b50f4ad4ced0c533ec4ca5055c705471c11ded6f
CoderHifzurRehman <hifzurrehman8199@gmail.com>
2023-05-28 20:39:42 +0530

    loader

--- src/components/common/Loader/index.jsx
+++ src/components/common/Loader/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Space, Spin } from "antd";
 import "./index.css";
+import LinkedinLoaderLogo from "../../../assets/LinkedinLoaderLogo.png";
 
 export default function Loader() {
   return (
     <div className="loader">
-      <p>Loading..Please Wait..</p>
+      <div className="loader-logo"><p>Linked</p><img className="linkedin-logo-loader" src={LinkedinLoaderLogo} alt="LinkedinLogo" /></div>
       <Space size="middle">
         <Spin size="large" />
       </Space>
